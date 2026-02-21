85eada1d82cab1ecc836cf2f6c2c9bc5f657531d
SamucaSupertramp <samucasupertramp@gmail.com>
2026-02-21 18:33:01 -0300

    superuser test3

--- back_end/users/apps.py
+++ back_end/users/apps.py
@@ -1,30 +1,4 @@
 from django.apps import AppConfig
-import os
-
-print("🔥 users.apps carregado")
 
 class UsersConfig(AppConfig):
     name = "users"
-
-    def ready(self):
-        print("🔥 ready() executado")
-        if os.environ.get("CREATE_SUPERUSER") != "true":
-            return
-
-        from django.contrib.auth import get_user_model
-
-        User = get_user_model()
-
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "samuel")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "samuelsattiro.dev@gmail.com")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "Cami1808")
-
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password,
-            )
-            print("✔ Superuser criado automaticamente")
-        else:
-            print("ℹ Superuser já existe")
