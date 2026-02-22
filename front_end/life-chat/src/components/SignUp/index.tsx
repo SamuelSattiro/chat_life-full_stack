a1ff8b37aaee8c98a91a27ba4e626041e95d8681
SamucaSupertramp <samucasupertramp@gmail.com>
2026-02-22 10:09:02 -0300

    settings config frontend final

--- front_end/life-chat/src/components/SignUp/index.tsx
+++ front_end/life-chat/src/components/SignUp/index.tsx
@@ -208,7 +208,7 @@ const SignUp = ({ onSwitch }: SignUpProps) => {
           <div>
             <button
               type="submit"
-              className="mt-6 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              className="mt-6 flex w-full cursor-pointer justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 [&:hover]:bg-indigo-400"
             >
               Cadastrar
             </button>
