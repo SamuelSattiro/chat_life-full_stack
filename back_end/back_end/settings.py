9083690500b55a91de048a118d8e6b5c62c0e988
SamucaSupertramp <samucasupertramp@gmail.com>
2026-02-22 14:40:30 -0300

    cloudinary conf.

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -159,4 +159,10 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.vercel.app",
 ]
 
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
